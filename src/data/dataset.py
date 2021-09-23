9608fc4556430ad7487d507f3c2eef5b390434e4
clement.leconte <clement.leconte@epita.fr>
2021-09-23 18:21:12 +0200

    [FIX] Build_dataset now works properly
    Need to let it run for a while to gather all the data we need for training

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -4,6 +4,7 @@ import csv
 import client
 import json
 import numpy as np
+from math import floor
 import matplotlib.pyplot as plt
 import pandas as pd
 import torch
@@ -19,7 +20,7 @@ def build_dataset():
     """Build dataset by downloading from Binance API"""
 
     # Get the current server time
-    server_time = client.CLIENT.get_server_time()
+    server_time = client.CLIENT.get_server_time()["serverTime"]
     for cp in crypto_pairs:
         print(cp)
         filename = 'src/data/csv' + cp + '.csv'
@@ -28,15 +29,23 @@ def build_dataset():
 
         # Get the first ever kline time
         first_kline = client.CLIENT.get_klines(symbol=cp, interval=client.CLIENT.KLINE_INTERVAL_1MINUTE, startTime=0, limit=1)
-        start_time = first_kline[0][1]
+        start_time = int(first_kline[0][0])
+        act_time = start_time
         steps = 1000
-        while start_time != server_time:
-            if (start_time + steps > server_time):
-                steps = server_time - start_time - 1
-            klines = client.CLIENT.get_klines(symbol=cp, interval=client.CLIENT.KLINE_INTERVAL_1MINUTE, startTime=start_time, limit=steps)  
+        percentage=0
+        total_steps = server_time - start_time
+        while act_time < server_time:
+            current_steps = act_time - start_time + 1
+            if (act_time + steps > server_time):
+                steps = server_time - act_time - 1
+            print(percentage)
+            if (percentage < floor((current_steps / total_steps) * 100)):
+                percentage = floor((current_steps / total_steps) * 100)
+                print(percentage, '% ')
+            klines = client.CLIENT.get_klines(symbol=cp, interval=client.CLIENT.KLINE_INTERVAL_1MINUTE, startTime=act_time, limit=steps)  
             for k in klines:
                 tmp.append([k[0], k[1], k[2], k[3], k[4], k[6]])
-        
+            act_time += steps
         with open(filename, 'w+', newline='') as csvfile:
             csv_writer = csv.writer(csvfile, delimiter=',')
             csv_writer.writerows(tmp)
